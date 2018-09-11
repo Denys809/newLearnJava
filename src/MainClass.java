3f5a4082ff97c6b1000d27e867ab3c3122d72767
Denys <denis809@bigmir.net>
2018-09-11 23:34:19 +0300

    comit_1

--- src/MainClass.java
+++ src/MainClass.java
@@ -110,6 +110,8 @@ public class MainClass {
 //
 //        dollar.setKursNBU(20);
 //        System.out.println(dollar.getKursNBU());
+
+//        dobavil kod!!!!!!+++++++++++++++++++++++++++++++++++++++!!!!!!!!!!!!!!!!!!
     }
 
 
