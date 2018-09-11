826be5f12728212b86d2db094a6f6a587a499fb9
Denys <denis809@bigmir.net>
2018-09-11 23:23:27 +0300

    Initial commit

---
+++ src/libs/WorkWithList.java
@@ -0,0 +1,15 @@
+package libs;
+
+
+import java.util.List;
+
+public class WorkWithList {
+    public void addNewList(List<List<String>> ourList) {
+
+    }
+
+
+    public List<List<String>> addValueToListList(int i, String test01, List<List<String>> ourList) {
+        return null;
+    }
+}
